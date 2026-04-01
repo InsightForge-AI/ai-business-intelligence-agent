618a2a014e2f3413b1ef400e4f24be1933b37c77
GURU VISHNU <107491799+guruvish@users.noreply.github.com>
2026-04-01 22:36:37 +0530

    Team c/rag data ingestion (#27)
    * Feat-Load,Ingest & Preprocessing the data
    
    * Document Ingestion & Preprocessing

---
+++ rag/Team_C/data/documents.py
@@ -0,0 +1,23 @@
+# Document Loading
+documents = [
+    {
+        "id": 1,
+        "text": "sales dropped due to bad delivery"
+    },
+    {
+        "id": 2,
+        "text": "customers complaining about late delivery"
+    },
+    {
+        "id": 3,
+        "text": "sales increased after marketing campaign"
+    },
+    {
+        "id": 4,
+        "text": "delivery delay caused customer dissatisfaction"
+    },
+    {
+        "id": 5,
+        "text": "product quality improved customer satisfaction"
+    }
+]
