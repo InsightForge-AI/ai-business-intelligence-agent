2965625dad4c60259a3ac65b728be10db5a436bc
KHAJA_MUBASHIR_ARSALAN <kmarsalan124@gmail.com>
2026-04-06 15:28:55 +0530

    sentiment analysis is done and tested

--- nlp/Team-A/app/main.py
+++ nlp/Team-A/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from app.api import summarize_api
+from app.api import summarize_api, sentiment_api
 
 app = FastAPI(title="AI NLP System")
 
+# Routers
 app.include_router(summarize_api.router, prefix="/summarize", tags=["Summarization"])
+app.include_router(sentiment_api.router, prefix="/sentiment", tags=["Sentiment"])
