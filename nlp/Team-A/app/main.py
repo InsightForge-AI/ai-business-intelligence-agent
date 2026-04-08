8906d3b2ebdfb10d049a2902373013ef9c953c1d
KHAJA_MUBASHIR_ARSALAN <kmarsalan124@gmail.com>
2026-04-08 18:38:31 +0530

    endpoint changed

--- nlp/Team-A/app/main.py
+++ nlp/Team-A/app/main.py
@@ -3,6 +3,6 @@ from app.api import summarize_api, sentiment_api, ke_api
 
 app = FastAPI(title="AI NLP System")
 
-app.include_router(summarize_api.router, prefix="/summarize", tags=["Summarization"])
-app.include_router(sentiment_api.router, prefix="/sentiment", tags=["Sentiment"])
-app.include_router(ke_api.router, prefix="/keywords", tags=["Keyword Extraction"])
+app.include_router(summarize_api.router, prefix="/nlp", tags=["Summarization"])
+app.include_router(sentiment_api.router, prefix="/nlp", tags=["Sentiment"])
+app.include_router(ke_api.router, prefix="/nlp", tags=["Keyword Extraction"])
