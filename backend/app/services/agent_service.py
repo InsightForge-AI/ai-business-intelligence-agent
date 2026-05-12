a2b05bb216dc407932dbdcd7bf0452eca11612a8
Vuthukota Narendra <tejunaren39@gmail.com>
2026-05-12 10:17:42 +0530

    Backend (#206)
    * Updated backend NLP and ML integrations
    
    * Updated backend
    
    ---------
    
    Co-authored-by: Vuthukota <tejunaren39@email.com>

--- backend/app/services/agent_service.py
+++ backend/app/services/agent_service.py
@@ -1,10 +1,21 @@
 import requests
 from config import AGENT_API_URL
 
-def run_agent(query):
-    response = requests.post(
-        AGENT_API_URL,
-        json={"query" : query}
-    )
 
-    return response.json()
+def run_agent(query):
+
+    try:
+
+        response = requests.post(
+            AGENT_API_URL,
+            json={"query": query},
+            timeout=15
+        )
+
+        response.raise_for_status()
+
+        return response.json()
+
+    except Exception:
+
+        return ["nlp"]
